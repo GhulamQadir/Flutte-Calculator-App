2d206198c44086eb0777f8150ae272f72071a399
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2021-06-14 03:32:09 -0700

    calc-in-progress

--- lib/calc.dart
+++ lib/calc.dart
@@ -10,12 +10,24 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   String result = "";
 
-  Widget btn(texttt) {
-    return ElevatedButton(
-        onPressed: () {
-          abc(texttt);
-        },
-        child: Text(texttt));
+
+  Widget btn(texttt){
+    return ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 50, height: 50),
+              child: ElevatedButton(onPressed: (){
+                abc(texttt);
+              }, 
+              child: Text(texttt),
+              style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              side: BorderSide(color: Colors.red)
+    )
+  )
+)
+              ),);
   }
 
   abc(texttt) {
@@ -28,6 +40,15 @@ class _CalculatorState extends State<Calculator> {
     setState(() {
       result = "";
     });
+  }
+  clearOne(){
+    setState(() {
+    result = result.substring(0, result.length - 1);
+    if(result == ""){
+      result = "0";
+    }
+
+        });
   }
 
   output() {
@@ -43,62 +64,107 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 300,
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Text(
-              "$result",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+            child: Text("Calculator App",
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 30,
+              fontWeight: FontWeight.bold
             ),
+            ),
+            ),
+          backgroundColor: Color(0xffdce0e6),
           ),
-          SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              btn("1"),
-              btn("2"),
-              btn("3"),
-              btn("+"),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              btn("4"),
-              btn("5"),
-              btn("6"),
-              btn("-"),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              btn("7"),
-              btn("8"),
-              btn("9"),
-              btn("*"),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              btn("0"),
-              ElevatedButton(onPressed: output, child: Text('=')),
-              ElevatedButton(onPressed: clearr, child: Text('Clear')),
-              btn("/"),
-            ],
-          ),
-        ],
-      ),
-    ));
+        body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Text(
+                "$result",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                btn("1"),
+                btn("2"),
+                btn("3"),
+                btn("+"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                btn("4"),
+                btn("5"),
+                btn("6"),
+                btn("-"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                btn("7"),
+                btn("8"),
+                btn("9"),
+                btn("*"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                btn("0"),
+         ConstrainedBox(
+                constraints: BoxConstraints.tightFor(width: 50, height: 50),
+                child: ElevatedButton(onPressed: output, child: Text('='),
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                side: BorderSide(color: Colors.red)
+      )))
+),),
+                ConstrainedBox(
+                constraints: BoxConstraints.tightFor(width: 50, height: 50),
+                child: ElevatedButton(onPressed: clearr, child: Text('Clear'),
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                side: BorderSide(color: Colors.red)
+      )))
+),),
+                btn("/"),
+         ConstrainedBox(
+                constraints: BoxConstraints.tightFor(width: 50, height: 50),
+                child: ElevatedButton(onPressed: clearOne, child: Text('Clear One'),
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                side: BorderSide(color: Colors.red)
+      )))
+),),              ],
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
